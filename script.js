fe058a4fdf0e41b898f6a06194a576c46d86de7b
Yash Parmar <yash20545@gmail.com>
2025-02-02 22:51:39 +0530

    dad joke genertor

---
+++ script.js
@@ -0,0 +1,21 @@
+let showjoke = document.getElementById('joke');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click',async() => {
+    try {
+        let url = `https://api.api-ninjas.com/v1/jokes`;
+        let apiKey = 't4A4pWuLqmLcuCVM0PL6/A==kU2IAGtK7aLNhNIu';
+
+        let headers = { "X-Api-Key": apiKey } ;
+        let response = await fetch(url, {
+            method: "GET",
+            headers: headers
+        });
+
+        let jokes = await response.json();
+        showjoke.innerText = jokes[0].joke;
+    } catch (err) {
+         console.error(err);
+    }
+})
+
